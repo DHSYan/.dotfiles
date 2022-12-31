601c44d6c999df59071e0e02e942e10b6934b09a
TwinkletoesZen <69187273+TwinkletoesZen@users.noreply.github.com>
2022-12-31 04:33:30 -0500

    Trying out bootstraping packer, good for using on other machine

---
+++ nvim/lua/twinkletoes/plugins/bootstrap-packer.lua
@@ -0,0 +1,89 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  use ('wbthomason/packer.nvim')
+
+  use ('nvim-lua/plenary.nvim')
+	use ('BurntSushi/ripgrep')
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+	use( 
+		'nvim-treesitter/nvim-treesitter'
+		, { run = ':TSUpdate'})
+
+
+	use ('nvim-treesitter/playground')
+
+	use ('theprimeagen/harpoon')
+	use ('ThePrimeagen/vim-be-good')
+
+	use('mbbill/undotree')
+
+	use('tpope/vim-fugitive')
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	}
+
+	use ('Pocco81/auto-save.nvim')
+
+	use ('lervag/vimtex') -- Latex
+
+	use ('ellisonleao/glow.nvim') -- markdown viewer
+
+	use ("terrortylor/nvim-comment")
+  
+  use ('nvim-lualine/lualine.nvim') -- Fancy Status bar
+
+  -- use ('lukas-reineke/indent-blankline.nvim') -- Indent line
+
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
--- nvim/lua/twinkletoes/plugins/init.lua
+++ nvim/lua/twinkletoes/plugins/init.lua
@@ -1,3 +1,7 @@
-pcall(require("twinkletoes.plugins.packer"))
+-- pcall(require("twinkletoes.plugins.packer"))
+--
+require("twinkletoes.plugins.bootstrap-packer")
 
 print("/lua/twinkletoes/plugins/init present")
+
+
