2513f7b8d799fa39b9e489473e4e535d23a22275
TwinkletoesZen <69187273+TwinkletoesZen@users.noreply.github.com>
2023-03-15 09:26:49 -0400

    disable tex TS highlighiting

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -23,7 +23,7 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    --disable = { "c", "rust" },
+    disable = { "latex", "tex" },
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     -- disable = function(lang, buf)
     --     local max_filesize = 100 * 1024 -- 100 KB
