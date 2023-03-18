d4050c1da8b450bd817dcba245b34a1b39e8e641
TwinkletoesZen <69187273+TwinkletoesZen@users.noreply.github.com>
2023-03-18 07:02:11 -0400

    Got telescope to show hidden files, and have consistent name binding to keymaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,10 +1,15 @@
 pcall(require('telescope').load_extension, 'fzf')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>g', builtin.git_files, {}) -- only search that files that are in git
--- vim.keymap.set('n', '<leader>ps', function()
+-- vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_files, {}) -- only search that files that are in git
+
+-- vim.keymap.set('n', '<leader>ws', function()
 -- 	builtin.grep_string ({ search = vim.fn.input("Grep > ") }) ;
 -- end)
 
-vim.keymap.set('n', '<leader>gs', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fs', function()
+	builtin.find_files ({ hidden = true}) ;
+end)
+
+vim.keymap.set('n', '<leader>ws', builtin.live_grep, {})
