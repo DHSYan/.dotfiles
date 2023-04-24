9ec13befd7ce72e78739292b3e4144bcdec82539
dhsyan <69187273+dhsyan@users.noreply.github.com>
2023-04-24 00:08:21 -0400

    Back to 80 colorcolumn, and new git useranme

--- nvim/lua/twinkletoes/Vim_Settings/set.lua
+++ nvim/lua/twinkletoes/Vim_Settings/set.lua
@@ -47,7 +47,7 @@ vim.g.rust_recommended_style = false
 
 vim.opt.scrolloff = 8 -- so you would never have less then 8 line below you
 
-vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = "80"
 
 -- vim.g.autochdir = true
 -- vim.opt.autochdir = true
