5036a653ce5badec75b6e4fecb9cbdeeea64de8d
DHSYan <sean.yan@uwaterloo.ca>
2023-09-25 18:33:35 +1000

    added new grid option for shkd, added better TeX vim Support, now opens in Skim app, and have image pasting worknig in tex as well

--- nvim/after/plugin/clipboard-image.lua
+++ nvim/after/plugin/clipboard-image.lua
@@ -5,6 +5,11 @@ require'clipboard-image'.setup {
     img_name = function() return os.date('%Y-%m-%d-%H-%M-%S') end, -- Example result: "2021-04-13-10-04-18"
     affix = "<\n  %s\n>" -- Multi lines affix
   },
+  tex = {
+    img_dir = "img",
+    img_name = function () return os.date('%Y-%m-%d-%H-%M-%S') end,
+    affix = "\\includegraphics{%s}"
+  },
   -- You can create configuration for ceartain filetype by creating another field (markdown, in this case)
   -- If you're uncertain what to name your field to, you can run `lua print(vim.bo.filetype)`
   -- Missing options from `markdown` field will be replaced by options from `default` field
