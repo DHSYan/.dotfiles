d5132cad8abf73528b36d12fce2a67518fcd3ff0
DHSYan <sean.yan@student.unsw.edu.au>
2023-11-21 20:18:03 +0800

    got programming snippets working

--- nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/lua/plugins/lsp/lsp-config.lua
@@ -29,12 +29,10 @@ local on_attach = function(_, bufnr)
     end
 
     nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
     nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
@@ -43,18 +41,15 @@ local on_attach = function(_, bufnr)
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
     -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
-
 
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
     end, { desc = 'Format current buffer with LSP' })
+end
+
+local luasnip_setup = function()
+
 end
 
 return {
@@ -64,7 +59,11 @@ return {
         'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
         'williamboman/mason-lspconfig.nvim',
         'williamboman/mason.nvim',
+        -- Snipnet Engine
         'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip', -- luasnip completion engine
+        -- Actual Snipnes
+        'rafamadriz/friendly-snippets',
     },
     config = function()
         -- local lspconfig = require("lspconfig")
@@ -94,6 +93,7 @@ return {
         -- Completion
         local cmp = require("cmp")
         local luasnip = require('luasnip')
+        require('luasnip.loaders.from_vscode').lazy_load()
         luasnip.config.setup({})
 
         cmp.setup({
@@ -103,11 +103,21 @@ return {
                     luasnip.lsp_expand(args.body) -- 0??          
                 end
             },
+            -- window = {
+            --     -- I think these make the window bordered?
+            --     -- Which should make them easier to see if that's the case
+            --     completion = cmp.config.window.bordered(),
+            --     documentation = cmp.config.window.bordered(),
+            -- },
+            completion = {
+                completeopt = 'menu,menuone,noinsert'
+            },
             mapping = cmp.mapping.preset.insert({
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                -- ['<C-Space>'] = cmp.complete({}), -- Invokes Completion
                 ['<CR>'] = cmp.mapping.confirm({
                     behavior = cmp.ConfirmBehavior.Replace, -- 0??
                     select = true                           -- 0??
@@ -134,4 +144,5 @@ return {
 -- Say for example, I added LuaSnip to the dependencies, 
 -- then what if there's configuration that needs to be configured for that
 -- plugin?
+
 
