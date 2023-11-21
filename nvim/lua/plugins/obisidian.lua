ed35489b46a161f839c4d3bc2f999786e03a8578
DHSYan <sean.yan@student.unsw.edu.au>
2023-11-21 20:13:29 +1100

    chore: cleaned some uncessary comments

--- nvim/lua/plugins/obisidian.lua
+++ nvim/lua/plugins/obisidian.lua
@@ -21,27 +21,17 @@ local opts = {
     },
 
     backlinks = {
-        -- The default height of the backlinks pane.
         height = 4,
-        -- Whether or not to wrap lines.
         wrap = true,
     },
 }
 
 return {
     "epwalsh/obsidian.nvim",
-    version = "*",  -- recommended, use latest release instead of latest commit
+    version = "*",  
     lazy = true,
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
     dependencies = {
-        -- Required.
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
         "hrsh7th/nvim-cmp",
