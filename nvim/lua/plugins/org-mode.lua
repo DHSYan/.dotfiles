58280fd29f3b1bf9e5709ce655f9ae1eab166bea
DHSYan <sean.yan@student.unsw.edu.au>
2023-11-24 00:30:09 +1100

    org mode

--- nvim/lua/plugins/org-mode.lua
+++ nvim/lua/plugins/org-mode.lua
@@ -19,8 +19,10 @@ return {
 
     -- Setup orgmode
     require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = {'~/orgfiles/**/*', '~/Library/CloudStorage/OneDrive-UNSW/1-Academics/Terms/23T3/Org/**/*'},
+      org_default_notes_file = '~/Library/CloudStorage/OneDrive-UNSW/1-Academics/Terms/23T3/Org/refile.org'
+            -- The refile org file can just live whereever I guess, but need
+            -- synced
     })
   end,
 }
