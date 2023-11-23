628c8e5873a7ce06a0405169a85109d1d1b69a4f
DHSYan <sean.yan@student.unsw.edu.au>
2023-11-23 17:37:27 +1100

    Better Pasting Image in OBsidian

--- nvim/lua/plugins/obisidian.lua
+++ nvim/lua/plugins/obisidian.lua
@@ -6,11 +6,11 @@ end
 
 
 -- TODO: These are Path on my Mac, need to make them more portable
-local concept_note_path =  "/Library/CloudStorage/OneDrive-UNSW/1-Academics/Concept-Notes"
+local concept_note_path = "/Library/CloudStorage/OneDrive-UNSW/1-Academics/Concept-Notes"
 local personal_icloud_path = "/Library/Mobile\\ Documents/com~apple~CloudDocs/Life-Life/Notes"
 local daily_notes_path = "/Library/CloudStorage/OneDrive-UNSW/1-Academics/Daily-Notes"
 
-local concept_note_path_half =  "~/Library/CloudStorage/OneDrive-UNSW/1-Academics/Concept-Notes"
+local concept_note_path_half = "~/Library/CloudStorage/OneDrive-UNSW/1-Academics/Concept-Notes"
 local personal_icloud_path_half = "~/Library/Mobile\\ Documents/com~apple~CloudDocs/Life-Life/Notes"
 local daily_notes_path_half = "~/Library/CloudStorage/OneDrive-UNSW/1-Academics/Daily-Notes"
 
@@ -42,23 +42,80 @@ local opts = {
         height = 4,
         wrap = true,
     },
+    completion = { -- This was taken from the docs
+        -- If using nvim-cmp, otherwise set to false
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars
+        min_chars = 2,
+        -- Where to put new notes created from completion. Valid options are
+        --  * "current_dir" - put new notes in same directory as the current buffer.
+        --  * "notes_subdir" - put new notes in the default notes subdirectory.
+        new_notes_location = "current_dir",
+
+        -- Control how wiki links are completed with these options:
+
+        -- Whether to add the note ID during completion.
+        -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
+        -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
+        prepend_note_id = true,
+
+        -- Whether to add the note path during completion.
+        -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
+        -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
+        prepend_note_path = false,
+
+        -- Whether to only use paths during completion.
+        -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
+        -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
+        use_path_only = false,
+    },
+
+    use_advanced_uri = true,
+
+    -- Specify how to handle attachments.
+    attachments = { -- This was taken from the docs
+        -- The default folder to place images in via `:ObsidianPasteImg`.
+        -- If this a relative path it will be interpreted as relative to the vault root.
+        -- You can always override this per image by passing a full path to the command instead of just a filename.
+        img_folder = "assets", -- This is the default
+        -- A function that determines the text to insert in the note when pasting an image.
+        -- It takes two arguments, the `obsidian.Client` and a plenary `Path` to the image file.
+        -- The is the default implementation.
+        ---@param client obsidian.Client
+        ---@param path Path the absolute path to the image file
+        ---@return string
+        img_text_func = function(client, path)
+            local link_path
+            local vault_relative_path = client:vault_relative_path(path)
+            if vault_relative_path ~= nil then
+                -- Use relative path if the image is saved in the vault dir.
+                link_path = vault_relative_path
+            else
+                -- Otherwise use the absolute path.
+                link_path = tostring(path)
+            end
+            local display_name = vim.fs.basename(link_path)
+            return string.format("![[%s]]", display_name, link_path)
+            -- Modified From the docs
+        end,
+    },
 }
 
 return {
     "epwalsh/obsidian.nvim",
     version = "*",
     lazy = true,
-    event = {
+    event = { -- this was taken from the docs
         -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
         -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-        "BufReadPre " .. vim.fn.expand("~") .. concept_note_path .. "/**.md" ,
-        "BufNewFile " .. vim.fn.expand("~") .. concept_note_path .. "/**.md" ,
+        "BufReadPre " .. vim.fn.expand("~") .. concept_note_path .. "/**.md",
+        "BufNewFile " .. vim.fn.expand("~") .. concept_note_path .. "/**.md",
 
-        "BufReadPre " .. vim.fn.expand("~") .. personal_icloud_path .. "/**.md" ,
-        "BufNewFile " .. vim.fn.expand("~") .. personal_icloud_path .. "/**.md" ,
+        "BufReadPre " .. vim.fn.expand("~") .. personal_icloud_path .. "/**.md",
+        "BufNewFile " .. vim.fn.expand("~") .. personal_icloud_path .. "/**.md",
 
-        "BufReadPre " .. vim.fn.expand("~") .. daily_notes_path .. "/**.md" ,
-        "BufNewFile " .. vim.fn.expand("~") .. daily_notes_path .. "/**.md" ,
+        "BufReadPre " .. vim.fn.expand("~") .. daily_notes_path .. "/**.md",
+        "BufNewFile " .. vim.fn.expand("~") .. daily_notes_path .. "/**.md",
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
@@ -71,7 +128,6 @@ return {
         obsidian.setup(opts);
 
         keymaps();
-
     end
 }
 -- Got the above from Obsidian Github
