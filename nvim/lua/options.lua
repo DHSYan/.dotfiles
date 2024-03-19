e58066232757086ceee01de60152af5472456fee
DHSYan <sean.yan@student.unsw.edu.au>
2024-03-20 10:17:22 +1100

    skhkd, xinit to start it, config for safe dir, and autofile detection for markdown's tabstop problem

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -62,3 +62,12 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+        vim.opt.tabstop = 2
+        vim.opt.softtabstop = 2
+        vim.opt.shiftwidth = 2
+    end,
+})
