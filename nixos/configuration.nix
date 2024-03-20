f69def755343a9e48c5ff4c24409b3cb405cb233
DHSYan <sean.yan@student.unsw.edu.au>
2024-03-21 03:13:07 +1100

    More stuff

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,6 +88,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
    environment.systemPackages = with pkgs; [
+     dmenu
+     wezterm
+     google-chrome
+     gcc
      neovim 
      wget
      git
