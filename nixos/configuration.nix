b4137395d98578026d2cc824162211922ca84b33
DHSYan <sean.yan@student.unsw.edu.au>
2024-04-05 00:20:13 +1100

    Got flakes running with my configuration nix file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
     ];
 
   console.useXkbConfig = true;
@@ -14,9 +14,9 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,29 +1,29 @@
 {
-  description = "Home Manager configuration of tzen";
+  description = "Flakes for Everything";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/23.11";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    #home-manager = {
+    #  url = "github:nix-community/home-manager";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."tzen" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        # extraSpecialArgs = {inherit inputs;};
+	modules = [ ./configuration.nix ];
       };
+      #homeConfigurations."tzen" = home-manager.lib.homeManagerConfiguration {
+      #  inherit pkgs;
+
+      #  modules = [ ./home.nix ];
+
+      #};
     };
 }
