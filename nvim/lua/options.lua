6979c71a925a39dfea4e76e8da0dd0b7e1e536f0
Sean Yan <z5521807@ad.unsw.edu.au>
2024-06-14 20:58:12 +1000

    change my git name to match the unis

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -75,8 +75,8 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "javascript",
     callback = function()
-        vim.opt.tabstop = 2
-        vim.opt.softtabstop = 2
-        vim.opt.shiftwidth = 2
+        vim.opt.tabstop = 4
+        vim.opt.softtabstop = 4
+        vim.opt.shiftwidth = 4
     end,
 })
