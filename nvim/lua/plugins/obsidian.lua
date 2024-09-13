d4e97229969042e6b759f177fd8e41f14b0acf28
Ding-Han Sean Yan <sean@dhsy.me>
2024-09-13 13:03:56 -0700

    obsidian: created the system of daily notes

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -24,7 +24,21 @@ local opts = {
         },
     },
 
-    new_notes_location = "notes_subdir",
+    daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "daily-notes",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%B %-d, %Y",
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { "daily-notes" },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = "daily-notes.md"
+    },
+
+
+    new_notes_location = "current_dir",
 
     note_id_func = function(title)
         return title
