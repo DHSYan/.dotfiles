b6fc248b4eb620733327292d29e1e963a704d623
Ding-Han Sean Yan <sean@dhsy.me>
2024-10-19 21:30:32 -0700

    feat: hypr ecosystem setup

--- hypr/start.sh
+++ hypr/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-swww init &
-swww img ~/.dotfiles/wallpaper/views/IMG_2646.JPG &
-
-nm-applet --indicator &
-
-waybar &
-
+# swww init &
+# swww img ~/.dotfiles/wallpaper/views/IMG_2646.JPG &
+#
+# nm-applet --indicator &
+#
+# waybar &
+#
