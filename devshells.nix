1936bea8f10b678ded3122d45c7d26a00364853d
DH Sean Yan <sean@dhsy.dev>
2025-11-22 12:29:32 -0800

    feat(devshell): a collection of shells I use

---
+++ devshells.nix
@@ -0,0 +1,32 @@
+{
+  description = "Dev Shells";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells = {
+
+          typst = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              typst
+              tinymist
+              # typst-lsp
+              typst-preview
+              typst-fmt
+            ];
+
+          };
+
+        };
+      });
+}
