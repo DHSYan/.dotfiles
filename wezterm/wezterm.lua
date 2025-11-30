6a1a9f33f77db0bdf5cd2ed7fd432a89fdd4ff78
DH Sean Yan <sean@dhsy.dev>
2025-11-29 16:54:43 -0800

    WIP: feat(wezterm)!: Attempt to make wezterm my terminal replacement
    need session management and resurrection for this to work, I want to
    main it because it has image generation, and maybe I can ditch obsidian

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,24 @@
 local wezterm = require 'wezterm'
-local config = {}
+local config = wezterm.config_builder()
 
 
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
--- config.font = wezterm.font 'ZedMono Nerd Font'
-config.font_size = 12.0
+-- config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font = wezterm.font 'ZedMono Nerd Font'
+config.font_size = 14.0
 
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 
-config.color_scheme = 'Teerb'
+-- config.color_scheme = 'Teerb'
+config.color_scheme = 'Gruvbox Dark (Gogh)'
 
 config.enable_wayland = true
 
 config.keys = require 'keys'
+config.enable_csi_u_key_encoding = true
+
+local sessions = wezterm.plugin.require("https://github.com/abidibo/wezterm-sessions")
+sessions.apply_to_config(config)
 
 return config
 
